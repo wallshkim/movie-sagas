850acbf4850085fe1f53984c9277e8391d787a72
Heather Walls <wallshkim@gmail.com>
2019-11-08 19:04:59 -0600

    Adds MovieItem component shell

---
+++ src/components/MovieItem/MovieItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class MovieItem extends Component {
+    // Renders the entire app on the DOM
+
+    render() {
+        return (
+            <div className="MovieItem">
+            </div>
+        );
+    }
+}
+
+export default MovieItem;
